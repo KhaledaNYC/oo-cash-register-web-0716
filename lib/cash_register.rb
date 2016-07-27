c51219cb3f005fd1261d7a2d70cb2857005a2e2b
Khaleda Khan <khaleda.khan87@gmail.com>
2016-07-27 22:28:03 +0000

    not sure why the answer should be 800 instead of 816

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total
+  def initialize(total = 0)
+    @total = total
+    @discount = discount
+  end
+  def discount
+    @discount = 20
+  end
+  def add_item(item,price,quantity = 1)
+    @price = price
+    @item = item
+    @quantity = quantity
+    @total += (@price * @quantity)
+  end
 
+  def apply_discount
+    percent_discount = discount.to_f / 100
+    actual_discount = total * percent_discount
+    @total =  total.to_f - actual_discount
+    @total
+  end
+end
