cf68f9a83fbaca53d8535b188857c6ccc3fc2f82
Khaleda Khan <khaleda.khan87@gmail.com>
2016-07-28 19:08:50 +0000

    fixed it!

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,32 @@
-require 'pry'
 class CashRegister
-  attr_accessor :total
-  def initialize(total = 0)
-    @total = total
-  end
-  def discount
-    @discount = 20
-  end
-  def add_item(item,price,quantity = 1)
-    @price = price
-    @item = item
-    @quantity = quantity
-    @total += (@price * @quantity)
-  end
-  def apply_discount
-    @total -= (@total * (self.discount)/100)
+  attr_accessor :total, :discount, :item, :cart, :receipt
+    def initialize(discount=20)
+      @total = 0
+      @discount = discount
+      @cart = []
+      @receipt = []
+    end
 
+    def add_item(item, price ,quantity = 1)
+      @item = item
+      cart << [item] * quantity
+      @receipt << price
+      self.total += (price * quantity)
+    end
+
+    def apply_discount
+       self.total -= (self.total * (self.discount)/100)
+       if self.total == 0
+         "There is no discount to apply."
+       else
+          "After the discount, the total comes to $#{self.total}."
+        end
+    end
+    def items
+      cart.flatten!
+    end
+    def void_last_transaction
+      self.total -= receipt.pop
+      self.total
+    end
   end
-end
